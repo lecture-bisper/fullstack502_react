cbb11741cb576513daf8cec4a9a45207e2319712
lecture-bisper <bisper@outlook.kr>
2025-08-26 17:01:43 +0900

    React - 개발환경 구성, console 환경에서 vite 로 react 프로젝트 생성

---
+++ reacttest1/reacttest2/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+        <h3>리액트 프로젝트 생성하기</h3>
+        <h4>리액트는 실시간으로 수정됨</h4>
+        <p>리액트 구동 후 바로 수정하면</p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
