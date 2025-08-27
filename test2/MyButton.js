d64a5594ad0b10215cb7768fe60eddbfa66f32c2
lecture-bisper <bisper@outlook.kr>
2025-08-27 16:44:07 +0900

    React - CDN으로 리액트 사용하기.

---
+++ test2/MyButton.js
@@ -0,0 +1,14 @@
+
+//  리액트 컴포넌트 선언
+function MyButton(props) {
+  const [isClicked, setIsClicked] = React.useState(false);
+
+  return React.createElement(
+    'button',
+    { onClick: () => setIsClicked(true) },
+    isClicked ? 'Clicked!' : 'Click here!!'
+  )
+}
+
+const domContainer = document.querySelector("#root");
+ReactDOM.render(React.createElement(MyButton), domContainer);
