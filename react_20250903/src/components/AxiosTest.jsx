fee9ee48c69186d239ef4a79ea2acf632f864068
lecture-bisper <bisper@outlook.kr>
2025-09-03 17:18:48 +0900

    React - 데이터 통신 - Axios 사용하기

---
+++ react_20250903/src/components/AxiosTest.jsx
@@ -0,0 +1,135 @@
+/// AxiosTest.jsx
+/// Date: 25. 9. 3.
+/// Time: 오후 2:58
+/// User: it
+/// 
+
+import axios from 'axios'
+
+//  axios : ajax 비동기 통신을 하기 위한 자바스크립트 라이브러리
+//  리액트는 Jquery 를 사용하지 않기 때문에 비동기 통신을 위한 axios 라이브러리가 필요함
+//  사용 시 해당 컴포넌트에 import 하여 사용
+//  단축 메소드를 제공하고 있음 (get, post, put, delete)
+//  then(콜백함수) : 비동기 통신이 성공 시 자동으로 동작하는 함수
+//  catch(콜백함수) : 비동기 통신이 실패 시 자동으로 동작하는 함수
+
+//  사용법 :
+//  설치 : > npm install axios;
+//  선언 : > import axios from 'axios';
+//  사용 :
+//    > axios.단축메소드(접속주소, 매개변수)
+//        .then(response => { 접속 성공 시 실행할 소스코드 ... })
+//        .catch(error => { 접속 실패 시 실행할 소스코드 ... });
+
+//    > axios({
+//        url: '접속주소',
+//        method: '통신 방식',
+//        data: {파라미터 값으로 전달할 데이터},
+//      });
+
+
+//    예시 : get/delete 의 사용방법이 동일함, post/put 의 사용방법이 동일함
+
+//    매개변수를 URL에 포함하여 axios 사용
+//    > axios.get('http://localhost:8080/login?id=bitc&pw=1234')
+//        .then(res => {성공 시 실행할 소스})
+//        .catch(err => {실패 시 실행할 소스});
+
+//    매개변수를 params 라는 속성에 추가하여 axios 사용
+//    > axios.get('http://localhost:8080/login', {
+//        params: {
+//          id: 'bitc',
+//          pw: '1234',
+//        },
+//      })
+//      .then(res => {성공 시 실행할 소스})
+//      .catch(err => {실패 시 실행할 소스});
+
+//    > axios.delete('http://localhost:8080/login', {
+//        params: {
+//          id: 'bitc',
+//          pw: '1234',
+//        },
+//      })
+//      .then(res => {성공 시 실행할 소스})
+//      .catch(err => {실패 시 실행할 소스});
+
+
+//    > axios.post('http://localhost:8080/login', null, {
+//        params: {
+//          id: 'bitc',
+//          pw: '1234',
+//        },
+//      }),
+//      .then(res => {성공 시 실행할 소스})
+//      .catch(err => {실패 시 실행할 소스});
+
+//    > axios.put('http://localhost:8080/login', null, {
+//        params: {
+//          id: 'bitc',
+//          pw: '1234',
+//        },
+//      }),
+//      .then(res => {성공 시 실행할 소스})
+//      .catch(err => {실패 시 실행할 소스});
+
+function AxiosTest () {
+
+  const getKobisData = () => {
+    axios.get('https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=c55013eadce1f0005fae142c556a228d&targetDt=20250902')
+      //  통신 성공 시 실행
+      .then(res => {
+        console.log('비동기 통신 성공!!');
+        
+        //  axios 를 통해서 전달받은 전체 데이터 가져오기
+        //  axios 를 통해서 전달받은 데이터는 object 타입으로 전달됨
+        //  config, data, headers, request, status, statusText 의 속성을 가지고 있음
+        //  서버에서 전달한 실제 데이터는 'data' 속성에 담겨 있음
+        // console.log(res);
+
+        //  전달받은 전체 데이터 중 필요한 데이터만 객체 분할 할당 방식으로 데이터를 가져옴
+        const { boxOfficeResult } = res.data;
+
+        //  객체 분할 할당 방식 사용
+        // const { boxofficeType, showRange, dailyBoxOfficeList } = boxOfficeResult;
+        // console.log(boxofficeType);
+        // console.log(showRange);
+        // console.log(dailyBoxOfficeList);
+
+        const { dailyBoxOfficeList } = boxOfficeResult;
+        // console.log(dailyBoxOfficeList);
+
+        //  ES6 의 배열 함수를 사용하여 내용 출력
+        dailyBoxOfficeList.forEach(item => {
+          let boxOfficeList = '';
+          boxOfficeList += '순위 : ' + item.rank + '\n';
+          boxOfficeList += `영화명 : ${item.movieNm}\n`;
+          boxOfficeList += `개봉일 : ${item.openDt}`;
+
+          console.log(boxOfficeList);
+        })
+      })
+      //  통신 실패 시 실행
+      .catch(err => {
+        console.log('비동기 통신 중 오류가 발생했습니다.');
+        console.log(err);
+      });
+  }
+
+  return (
+    <div>
+      <button type={'button'} className={'btn btn-primary'} onClick={getKobisData}>클릭 시 데이터 가져오기</button>
+    </div>
+  )
+}
+
+export default AxiosTest
+
+
+
+
+
+
+
+
+
